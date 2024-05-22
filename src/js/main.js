6f140a5981f847fae1f376adacc31b32f7a5e078
NikolayShavar <fargon45821@gmail.com>
2024-05-22 18:34:47 +0300

    hero+spons (adaptive) done

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,23 @@
-$(".center").slick({
-  centerMode: true,
-  centerPadding: "120px",
-  rows: 2,
-  initialSlide: 1,
-  slidesToShow: 2,
+$('.autoplay').slick({
+  slidesToShow: 5,
   slidesToScroll: 1,
-  prevArrow: $(".choose-arrows__left"),
-  nextArrow: $(".choose-arrows__right"),
-  infinite: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+
   responsive: [
     {
-      breakpoint: 720,
+      breakpoint: 1270,
       settings: {
-        centerPadding: "10px",
-        rows: 1,
-      },
+        slidesToShow: 4,
+      }
     },
     {
-      breakpoint: 1280,
-      settings: {
-        rows: 1,
-        slidesToShow: 2,
-        centerPadding: "100px",
-        arrows: false,
-      },
-    },
-  ],
-});
-
-$(".center-row").slick({
-  centerMode: true,
-  centerPadding: "330px",
-  initialSlide: 1,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: $(".choose-combo-arrows__left"),
-  nextArrow: $(".choose-combo-arrows__right"),
-  infinite: false,
-  responsive: [
-    {
-      breakpoint: 1680,
+      breakpoint: 575,
       settings: {
         slidesToShow: 2,
-        centerPadding: "250px",
-      },
-    },
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 2,
-        centerPadding: "100px",
-      },
-    },
-    {
-      breakpoint: 760,
-      settings: {
-        slidesToShow: 1,
-        centerPadding: "50px",
-      },
-    },
 
-    
-  ],
-});
-
-$(".center").on("afterChange", function (event, slick, currentSlide) {
-  $(".choose-arrows__left").toggle(currentSlide !== 0);
-  $(".choose-arrows__right").toggle(currentSlide !== slick.slideCount - 1);
-});
-
-$(".center-row").on("afterChange", function (event, slick, currentSlide) {
-  $(".choose-combo-arrows__left").toggle(currentSlide !== 0);
-  $(".choose-combo-arrows__right").toggle(currentSlide !== slick.slideCount - 1);
+      }
+    }
+  ]
 });
